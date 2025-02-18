497db525142ee639a9655e3dd7e4e35f639df66e
alban <manzanoalban@gmail.com>
2025-02-18 22:50:36 +0100

    Fairy follows last interest point

--- Scenes/InterestPoint/InterestPoint.cs
+++ Scenes/InterestPoint/InterestPoint.cs
@@ -3,14 +3,26 @@ using System;
 
 public partial class InterestPoint : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+    [Signal]
+    public delegate void OnPointActivatedEventHandler(InterestPoint point);
+
+
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
 	{
         AddToGroup("InterestPoint");
+        Area2D collision = GetNode<Area2D>("Area2D");
+        collision.BodyEntered += OnBodyEntered;
     }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+    protected void OnBodyEntered(Node2D body)
+    {
+         EmitSignal(SignalName.OnPointActivated, this);
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
 	{
 	}
 }
