6845451d9f650b66d5f1a35d7d972faefcc1f283
BIP-BOUP\yannl <mrplobi@gmail.com>
2025-02-18 17:11:25 +0100

    Button visual integrated

--- Scenes/Button.cs
+++ Scenes/Button.cs
@@ -1,7 +1,8 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
-public partial class Button : Node
+public partial class Button : Sprite2D
 {
     [Signal]
     public delegate void ButtonPressedEventHandler();
@@ -10,16 +11,27 @@ public partial class Button : Node
 
     int pressers = 0;
 
+    public override void _Ready()
+    {
+        pressers = 0;
+    }
+
     public void OnButtonPressed(Node2D body)
     {
         pressers++;
         if( pressers == 1 )
+        {
+            Frame = 0;
             EmitSignal(SignalName.ButtonPressed);
+        }   
     }
     public void OnButtonReleased(Node2D body)
     {
         pressers--;
         if (pressers == 0)
+        {
+            Frame = 1;
             EmitSignal(SignalName.ButtonReleased);
+        }
     }
 }
