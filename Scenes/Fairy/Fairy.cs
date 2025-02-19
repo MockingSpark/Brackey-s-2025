8c9788a53bbddd00aef03c6c7012c3b7e4e74639
BIP-BOUP\yannl <mrplobi@gmail.com>
2025-02-19 14:21:55 +0100

    New fairy visual and box collison fix

--- Scenes/Fairy/Fairy.cs
+++ Scenes/Fairy/Fairy.cs
@@ -78,6 +78,8 @@ public partial class Fairy : Node2D
 
         Vector2 position = currentPointFollowed == null ? player.GlobalPosition - playerFollowPoint : currentPointFollowed.GlobalPosition;
 
+
+
         GlobalPosition = GlobalPosition.Lerp(position, (float)(Speed * delta));
 	}
 
