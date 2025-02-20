2fc34f5351764abaeaaff3994d5c015066e512ff
MockingSpark <mockingspark@gmail.com>
2025-02-20 22:42:55 +0100

    level design almost done

--- Scenes/Door/Door.cs
+++ Scenes/Door/Door.cs
@@ -10,31 +10,56 @@ public partial class Door : Node2D
 	public bool isVertical = true;
 	[Export]
 	public bool movesLeft = false;
+	[Export]
+	public bool invert = false;
 
 	Vector2 destination = new Vector2();
 	bool shouldMove = false;
 
-    public override void _Ready()
-    {
-		closePosition = GlobalPosition;
-		if (isVertical)
+	public override void _Ready()
+	{
+		if (invert)
 		{
-			openPosition = GlobalPosition + new Vector2(0, 124);
+			openPosition = GlobalPosition;
+			if (isVertical)
+			{
+				closePosition = GlobalPosition + new Vector2(0, -124);
+			}
+			else
+			{
+				if (movesLeft)
+				{
+					closePosition = GlobalPosition + new Vector2(124, 0);
+				}
+				else
+				{
+					closePosition = GlobalPosition + new Vector2(-124, 0);
+				}
+			}
 		}
 		else
 		{
-			if (movesLeft)
-            {
-                openPosition = GlobalPosition + new Vector2(-124, 0);
-            }
+			closePosition = GlobalPosition;
+			if (isVertical)
+			{
+				openPosition = GlobalPosition + new Vector2(0, 124);
+			}
 			else
-            {
-                openPosition = GlobalPosition + new Vector2(124, 0);
-            }
+			{
+				if (movesLeft)
+				{
+					openPosition = GlobalPosition + new Vector2(-124, 0);
+				}
+				else
+				{
+					openPosition = GlobalPosition + new Vector2(124, 0);
+				}
+			}
 		}
-    }
+		
+	}
 
-    public void OpenDoor()
+	public void OpenDoor()
 	{
 		destination = openPosition;
 		shouldMove = true;
