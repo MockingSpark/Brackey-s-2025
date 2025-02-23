e3881846e13fdc848c9a366118e02372eb8dca25
Kwakwaouaite <pamart.pierrick@hotmail.fr>
2025-02-23 09:30:39 +0100

    Can size bubble in world

--- Scenes/Dialogue/Dialogue.cs
+++ Scenes/Dialogue/Dialogue.cs
@@ -11,13 +11,13 @@ public partial class Dialogue : Resource
     [Export] public bool isBold = true;
     [Export] public bool linkedToFairy = true;
     [Export] public Vector2 position = new Vector2();
+    [Export] public float bubbleScaleForInWorld = 1.0f;
 
     [Export] protected Array<EBlackboardKey> arguments = new Array<EBlackboardKey>();
 
     public string GetText() 
     {
         Array<string> strings = new Array<string>();
-        
         foreach(EBlackboardKey key in arguments)
         {
             strings.Add(Blackboard.Instance.GetValueAnyBoard(key,0).ToString());
